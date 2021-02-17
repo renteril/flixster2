a8a2364ad5c96a217ba75291ff3dfba799c07c5c
renteril <renteril@oregonstate.edu>
2021-02-16 20:08:44 -0800

    Initial Commit

---
+++ flixster2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster2
+//
+//  Created by Luis Renteria on 2/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
