5aae40c1641c2973811171a1b72df49bce42d14a
renteril <renteril@oregonstate.edu>
2021-02-25 20:18:25 -0800

    Update: Flix - Collection View Layout

---
+++ flixster2/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster2
+//
+//  Created by Luis Renteria on 2/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
